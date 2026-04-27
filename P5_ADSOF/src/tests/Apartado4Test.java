6711395a468b714f6a13a3beac204a75320fe3cb
dunap <dunap@LAPTOP-2VAK2E2V>
2026-04-27 17:26:34 +0200

    Test del apartado 4 sin acabar

--- P5_ADSOF/src/tests/Apartado4Test.java
+++ P5_ADSOF/src/tests/Apartado4Test.java
@@ -1,5 +1,39 @@
 package tests;
 
-public class Apartado4Test {
+import dataset.*;
+import decision_tree.*;
 
+public class Apartado4Test {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+	public static DecisionTree<Weather> learnTree(){
+		LabeledDataSet<Weather, Boolean> dataSet = buildDataSet();
+		GreedyTreeLearner<Weather, Boolean> learner = new GreedyTreeLearner<>();
+		DecisionTree<Weather> tree = learner.learn(dataSet);
+		return tree;
+	}
+	
+	private static LabeledDataSet<Weather, Boolean> buildDataSet(){
+		Weather conditions[] = {
+				new Weather(WeatherCondition.RAINY, Temperature.COLD),
+				new Weather(WeatherCondition.RAINY, Temperature.HOT),
+				new Weather(WeatherCondition.SUNNY, Temperature.MILD),
+				new Weather(WeatherCondition.CLOUDY, Temperature.COOL),
+				new Weather(WeatherCondition.WINDY, Temperature.WARM),
+				new Weather(WeatherCondition.SUNNY, Temperature.SCORCHING),
+				new Weather(WeatherCondition.SNOWY, Temperature.FREEZING),
+				new Weather(WeatherCondition.SANDSTORM, Temperature.HOT),
+				new Weather(WeatherCondition.RAINY, Temperature.COLD),
+				new Weather(WeatherCondition.RAINY, Temperature.WARM)
+	};
+		
+		LabeledDataSet<Weather, Boolean> ds = new LabeledDataSet<>(new WeatherFeaturizer(), new ShouldIPlayTennisToday());
+		ds.addAll(conditions);
+		return ds;
+		
+	}
 }
--- P5_ADSOF/src/tests/ShouldIPlayTennisToday.java
+++ P5_ADSOF/src/tests/ShouldIPlayTennisToday.java
@@ -6,7 +6,8 @@ public class ShouldIPlayTennisToday implements LabelProvider<Weather, Boolean>{
 
 	@Override
 	public Boolean asignarEtiqueta(Weather w) {
-		return (w.getCondition() == WeatherCondition.SUNNY || w.getCondition() == WeatherCondition.CLOUDY) && 
+		return (w.getCondition() == WeatherCondition.SUNNY || w.getCondition() == WeatherCondition.CLOUDY 
+				|| w.getCondition() == WeatherCondition.CLOUDY) && 
 				(w.getTemperature() == Temperature.COOL || w.getTemperature() == Temperature.MILD ||
 				 w.getTemperature() == Temperature.WARM);
 	}
